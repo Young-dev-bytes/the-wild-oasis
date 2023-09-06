ce655fde64420d99d290c9049e0a367043770ec2
chenqiuyang <1182492999@qq.com>
2023-09-06 19:22:17 +0800

    0906

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,29 @@
+import { styled } from "styled-components";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
+const Button = styled.button`
+  font-size: 15px;
+  background-color: purple;
+`;
+
+const StyledApp = styled.div`
+  background-color: orange;
+  padding: 10px;
+`;
+
 function App() {
-  return <div>hello</div>;
+  return (
+    <StyledApp>
+      <H1>hello</H1>
+      <Button>Check in</Button>
+      <Button>Check out</Button>
+    </StyledApp>
+  );
 }
 
 export default App;
